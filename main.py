b8aa32cd93e439e39de34dc279839ea1d2c4b8c2
oguzhangc <58819819+ogzcode@users.noreply.github.com>
2022-12-19 20:09:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-def isOdd(number):
-  return number % 2 == 0
+def Encryption(plaintext, key_val):
+    ciphertext = ''
+    for i in range(len(plaintext)):
+        special = plaintext[i]
+        new_special = special.lower()
+        if new_special == " ":
+            ciphertext += ' '
+        elif special.isalpha():
+            ciphertext += chr((ord(new_special) + key_val - 97) % 26 + 97)
 
-number = input("Bir sayı giriniz")
+    return ciphertext
 
-if isOdd(number):
-  print("Sayı tektir")
-  
+while True:
+    print(
+        'Welcome to my Word..\n [*] Press 1 for Encryption \n [*] Press 0 for Decryption \n [*] Press 01 to exit.. ')
+    print('Tip ---> Encryption/Decryption with shift value of your choice ! ')
+    choice = input('Insert Here : ')
+    if choice.isdigit():
+        if choice == '1':
+            sen = input('Insert the plaintext : ')
+            key = int(input('Insert shift value(Only integer values) : '))
+            print(50 * '-')
+            print(f'Your ciphertext ---> {Encryption(sen, key)}')
+            print(50 * '-')
+            print('Special symbols (!,# etc and numbers) are deleted..')
+            con = input('Shall we continue ? [Any Key/no]')
+            if con == 'no':
+                print('Exiting..')
+                break
+            else:
+                pass
+        elif choice == '01':
+            print('Exiting..')
+            break
+        else:
+            print('Exception error .. \n'
+                  'Please insert 0 or 1 ')
